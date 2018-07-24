41a6571aca711d87538b006ab07c9cd6782b484e
alexjmcm <alexjmcm@gmail.com>
2018-07-24 00:36:07 -0400

    main functions created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,75 @@
 // Global Variables
 
 var random_result;
-var lost;
-var win;
+var lost = 0;
+var win = 0;
+var previous = 0;
 
 // Setters
 // Getters
 
 $()
 
-random_result= Math.floor(Math.random() * 102 + 18);
-console.log(random_result);
-$("#result").html('Random Result: ' + random_result);
-for(var i = 0; i < 4; i++)
-{
-    var random = Math.floor(Math.random() * 11) + 1; 
-    console.log(random);
-    var crystal = $( "<div>");
-        crystal.attr({
-        "class": 'crystal',
-        "data-random": random
-        });
+var resetAndStart = function () {
+    $(".crystals").empty();
+
+    random_result= Math.floor(Math.random() * 102 + 18);
+    console.log(random_result);
+    $("#result").html('Random Result: ' + random_result);
+    for(var i = 0; i < 4; i++)
+    {
+        var random = Math.floor(Math.random() * 11) + 1; 
+        console.log(random);
+        var crystal = $( "<div>");
+            crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+            });
       
     $(".crystals").append(crystal);
 
 }
+ $("#previous").html("Total Score:" + previous);
+}
 
-$(".crystal").on('click', function () {
-    console.log($(this).attr('data-random'));
+
+
+resetAndStart();
+
+// var reset = function () {
+    
+// }
+
+
+
+$(document).on('click', ".crystal", function () {
+
+    var num = parseInt($(this).attr('data-random'));
+
+    previous += num;
+
+    $("#previous").html("Total Score:" + previous);
+
+    if(previous > random_result) {
+        console.log("You Lost!!");
+        lost++;
+        $("#lost").html("You Lost:" + lost);
+        previous = 0
+       
+        resetAndStart();
+
+    }
+    else if(previous === random_result){
+        console.log("You Win");
+        win++;
+
+        $("#win").html("You Win:" + win);
+      
+        previous = 0
+        resetAndStart();
+    }
+
+    console.log(previous);
 
 });
 
